8386b566c7cdfcb3fcedd7fe23297b1598277b0b
Kevin S. Hennessey <kevinh@xavient.com>
2018-09-13 16:33:20 -0400

    added json-server to run with command start-api

--- src/sagas/sessionSaga.js
+++ src/sagas/sessionSaga.js
@@ -5,20 +5,27 @@ import { takeLatest, call, put} from 'redux-saga/effects';
 //import { push } from 'react-router-redux';
 
 function loginApi(authParams) {
-  return {
-    profileStatus : 'admin',
-    username: 'sampleUser',
-  }
+  // return fetch('http://localhost:3000/user',
+  //   {
+  //       method: "GET",
+  //       headers: {
+  //       }
+  //   })
+  //   .then(response => response.json())
+  //   .then(json => console.log(json))
 }
 
 function* loginEffectSaga(action) {
   try{
-    const profileStatus = yield(call(loginApi, action.payload))
-    //const profileStatus = loginApi(null);
-    yield put(updateProfile(profileStatus));
+    const response = yield fetch('http://localhost:3000/user')
+      .then(response => response.json())
+    console.log(response)
+    //const profileStatus = yield(call(loginApi, action.payload))
+    //console.log(profileStatus);
+    yield put(updateProfile(response[0]));
     //yield put(push('/'));
   } catch (e) {
-    console.log("the login call failed")
+    console.log("the login call failed: "+ e)
   }
 }
 
